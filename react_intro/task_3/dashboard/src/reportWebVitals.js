da180d2d034893e3e8f36a8ada35385d5691a687
agigibairene <i.agigiba@alustudent.com>
2025-01-14 22:51:36 +0200

    task 3

--- react_intro/task_3/dashboard/src/reportWebVitals.js
+++ react_intro/task_3/dashboard/src/reportWebVitals.js
@@ -1,13 +1,4 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-export default reportWebVitals;
+configure({ adapter: new Adapter() });
