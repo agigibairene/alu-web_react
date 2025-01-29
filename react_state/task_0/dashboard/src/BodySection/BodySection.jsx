6afe7f2a6aa087c49349069f9ddccb8c4bb67a6f
agigibairene <i.agigiba@alustudent.com>
2025-01-30 00:54:28 +0200

    react state

---
+++ react_state/task_0/dashboard/src/BodySection/BodySection.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+export default function BodySection({ title, children }) {
+  return (
+    <div className="bodySection">
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+}
